1545144014e08931f0130310cd820a643f471223
danihenrif <danielhrqs4@yahoo.com>
2024-03-15 18:58:19 -0300

    implementação final socket com chatbot aiml

--- client/chatbotclient/src/main/java/com/chatbot/Main.java
+++ client/chatbotclient/src/main/java/com/chatbot/Main.java
@@ -1,24 +1,40 @@
 package com.chatbot;
 
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Date;
-
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      try {
-        try (Socket cliente = new Socket("",1235)) {
-            ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-            Date data_atual = (Date)entrada.readObject();
-            JOptionPane.showMessageDialog(null,"Data recebida do servidor:" + data_atual.toString());
-            entrada.close();
-        }
-        System.out.println("Conexão encerrada");
-      }
-      catch(Exception e) {
-        System.out.println("Erro: " + e.getMessage());
-      }
+        try {
+            Socket clienteSocket = new Socket("localhost", 1235);
+            
+            ObjectOutputStream saida = new ObjectOutputStream(clienteSocket.getOutputStream());
+            ObjectInputStream entrada = new ObjectInputStream(clienteSocket.getInputStream());
+            
+			try (Scanner scanner = new Scanner(System.in)) {
+				String pergunta = "";
+				
+				while(!pergunta.equals("q")) {
+					System.out.println("Você: ");
+					pergunta = scanner.nextLine();
+					
+					saida.writeObject(pergunta);
+					saida.flush();
+
+				    String resposta = (String) entrada.readObject();
+				    System.out.println("Chatbot: " + resposta);
+
+				}
+
+			    saida.close();
+			    entrada.close();
+			    clienteSocket.close();
+			}
+	    } 
+        catch (Exception e) {
+        	System.out.println("Erro: " + e.getMessage());
+	    }
     }
-  }
+}
--- server/chatbotserver/src/main/java/com/chatbot/ServerMain.java
+++ server/chatbotserver/src/main/java/com/chatbot/ServerMain.java
@@ -1,25 +1,44 @@
 package com.chatbot;
 
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
 
 public class ServerMain {
+    static Chatbot chatbot = new Chatbot();
     public static void main(String[] args) {
         try {
-            // Instancia o ServerSocket ouvindo a porta 12345
+            @SuppressWarnings("resource")
             ServerSocket servidor = new ServerSocket(1235);
             System.out.println("Servidor ouvindo a porta 1235");
+
             while (true) {
-                // o método accept() bloqueia a execução até que
-                // o servidor receba um pedido de conexão
                 Socket cliente = servidor.accept();
                 System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
+
                 ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-                saida.flush();
-                saida.writeObject(new Date());
+                ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
+                
+                boolean continuaConversa = true;
+                while(continuaConversa) {
+                	String mensagem = (String) entrada.readObject();
+                    System.out.println("Pergunta do cliente: " + mensagem);
+           
+	                if (mensagem.equals("q")) {
+	                	String resposta = "Fechando a sua conexão...";
+	                    saida.writeObject(resposta);
+	                    saida.flush();
+	                    
+	                } else {
+	                    String resposta = Chatbot.main(null, mensagem);
+	                    saida.writeObject(resposta);
+	                    saida.flush();
+	                }
+                }
                 saida.close();
+                entrada.close();
+                cliente.close();
             }
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
