179a4eeefe38059b9259690ad1a5a063360c72f4
Andre Soares <andrevms@hotmail.com>
2024-03-15 23:35:12 -0300

    Refactor : Modularização chatbot e socket

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+L
+server/chatbotserver/pom.xml,0\9\09566211445c33bae4f54487255ac374857cc61e
+L
+client/chatbotclient/pom.xml,0\2\02d4e904da57ce6c6128a54c1de4778c383ffb4c
+~
+Nserver/chatbotserver/src/main/java/com/chatbot/socket/ServerSocketChatbot.java,7\a\7a29d274d54c0e255e931c756d7cdaa4cf1748a5
+w
+Gclient/chatbotclient/src/main/java/com/chatbot/socket/ClientSocket.java,a\9\a9677341707dc3e63d7ca453989f4bde4aed1f30
+h
+8server/chatbotserver/src/main/java/com/chatbot/Main.java,d\4\d43fc5b4ff53cd2ef0bd9792c752e2b9f7dfe01e
+h
+8client/chatbotclient/src/main/java/com/chatbot/Main.java,7\f\7fc8890fd6110a2a764836c343ffd43a50e8e12e
--- client/chatbotclient/src/main/java/com/chatbot/Main.java
+++ client/chatbotclient/src/main/java/com/chatbot/Main.java
@@ -1,7 +1,30 @@
 package com.chatbot;
 
+import com.chatbot.socket.ClientSocket;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ClientSocket client = new ClientSocket();
+        try {
+            client.startConnection("localhost", 1235);
+
+            Scanner scanner = new Scanner(System.in);
+            String pergunta = "";
+
+            while (!pergunta.equals(".")) {
+                System.out.println("Você: ");
+                pergunta = scanner.nextLine();
+
+                String response = client.sendMessage(pergunta);
+                System.out.println("Chatbot: " + response);
+
+            }
+            client.stopConnection();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
